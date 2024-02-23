9027a429eb17e93b407e7626a2a1cc780bfe1f2f
sPOofy_rANSOmWARe <126998387+sPOofy-rANSOmWARe@users.noreply.github.com>
2024-02-23 16:43:29 +0530

    Add files via upload

---
+++ Python_task/Main function.py
@@ -0,0 +1,23 @@
+import latex_to_aiken
+import subprocess
+import platform
+import os
+
+if __name__=="__main__":
+    try:
+        #tex source filename
+        #file=input("Enter file name without .tex: ")
+        #tex_filename=file+'.tex'
+        tex_filename='tex_input.tex'
+        output_file = latex_to_aiken.convert(tex_filename)
+        print("File saved with name : " + output_file)
+        # open PDF with platform-specific command
+        if platform.system().lower() == 'windows':
+            os.startfile(output_file)
+        elif platform.system().lower() == 'linux':
+            subprocess.run(['xdg-open', output_file])
+        else:
+            raise RuntimeError('Unknown operating system "{}"'.format(platform.system()))
+    except:
+        print("Some Error Occured")
+
