6d9efad7ba6539aec49f74d31e713dd0e765a43c
pranjul_m <126998387+sPOofy-rANSOmWARe@users.noreply.github.com>
2024-02-24 17:05:06 +0530

    Update latex_to_aiken.py

--- Python_task/latex_to_aiken.py
+++ Python_task/latex_to_aiken.py
@@ -4,7 +4,6 @@ import subprocess
 def convert(tex_filename):
     filename, ext = os.path.splitext(tex_filename)
     pdf_filename = filename + '.pdf'
-    # pdflatex helps in rendering the content of Latex file
     subprocess.run(['pdflatex', '-interaction=nonstopmode', tex_filename])
     # check if PDF is successfully generated
     if not os.path.exists(pdf_filename):
